7e9e2c15cd1b7fbf45c837798636e8c6f873cb5f
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-06-29 21:37:06 +0200

    Fix peek launcher module path

--- peek/info.rkt
+++ peek/info.rkt
@@ -1,6 +1,8 @@
 #lang info
 
+(define collection 'multi)
+
 (define deps '("base" "peek-lib" "peek-doc"))
 
 (define racket-launcher-names '("peek"))
-(define racket-launcher-libraries '("main.rkt"))
+(define racket-launcher-libraries '("peek/main.rkt"))
